2a3489f1cff7b7d7d542617787c8a44cf4d49b26
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-01-07 02:53:16 -0800

    add abis
    listen for new NFTs and post to discord channel
    
    
    move key to .env

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,18 @@
 import * as dotenv from "dotenv";
 import { Client, Intents } from "discord.js";
+import { ethers } from "ethers";
+import { NudeNFT__factory } from "./typechain/factories/NudeNFT__factory.js";
 dotenv.config();
+const infuraProvider = `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API_KEY}`;
+const NUDENFT_ADDRESS = '0x85E9dd3521B9c2Fd7AE9B6C4C9b394f040Bea136';
+const provider = new ethers.providers.JsonRpcProvider(infuraProvider);
+const contract = NudeNFT__factory.connect(NUDENFT_ADDRESS, provider);
 const client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES]
 });
+const CHANNEL_IDS = {
+    nudeBot: "926198540240289863"
+};
 client.once("ready", () => {
     client.on('guildMemberAdd', (member) => {
         member.send(`Hey qt, I am NudeBot! The custom bot for Own Me and our $NUDE token. Happy to have you here! Don't be shy!\n\nPlease take my welcome package to get you started in our kinky and wild NFT marketplace. I'm sure you'll find something to peak your interest! :stuck_out_tongue_winking_eye:\n\nhttps://ownme.io\n\nBye now! See you around in the xxx-metaverse! :candy:`);
@@ -11,6 +20,19 @@ client.once("ready", () => {
     client.on('messageCreate', (message) => {
     });
     client.on('interactionCreate', (interaction) => {
+    });
+    contract.on(contract.filters.newNFTMinted(), (recipient, tokenId, tokenURI, price, event) => {
+        const parsedTokenURI = JSON.parse(tokenURI);
+        client?.channels?.cache?.get(CHANNEL_IDS.nudeBot)?.send({
+            content: `**${parsedTokenURI.title}**\n\n${parsedTokenURI.description}\n\n${ethers.utils.formatEther(price)} NUDE\n\nhttps://mumbai.polygonscan.com/tx/${event.transactionHash}\n\n${parsedTokenURI.image}`,
+            embeds: [
+                {
+                    thumbnail: {
+                        url: parsedTokenURI.image,
+                    }
+                }
+            ]
+        });
     });
     console.log("NudeBot Ready!");
 });
--- index.ts
+++ index.ts
@@ -1,11 +1,23 @@
 import * as dotenv from "dotenv";
-import { Client, Intents } from "discord.js";
+import { Client, Intents, TextChannel } from "discord.js";
+import { ethers } from "ethers";
+import { NudeNFT__factory } from "./typechain/factories/NudeNFT__factory.js";
 
 dotenv.config();
+
+const infuraProvider = `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API_KEY}`;
+const NUDENFT_ADDRESS = '0x85E9dd3521B9c2Fd7AE9B6C4C9b394f040Bea136';
+
+const provider = new ethers.providers.JsonRpcProvider(infuraProvider);
+const contract = NudeNFT__factory.connect(NUDENFT_ADDRESS, provider);
 
 const client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES]
 });
+
+const CHANNEL_IDS = {
+    nudeBot: "926198540240289863"
+}
 
 client.once("ready", () => {
     client.on('guildMemberAdd', (member) => {
@@ -16,6 +28,21 @@ client.once("ready", () => {
     });
 
     client.on('interactionCreate', (interaction) => {
+
+    });
+
+    contract.on(contract.filters.newNFTMinted(), (recipient, tokenId, tokenURI, price, event) => {
+        const parsedTokenURI = JSON.parse(tokenURI);
+        (client?.channels?.cache?.get(CHANNEL_IDS.nudeBot) as TextChannel)?.send({
+            content: `**${parsedTokenURI.title}**\n\n${parsedTokenURI.description}\n\n${ethers.utils.formatEther(price)} NUDE\n\nhttps://mumbai.polygonscan.com/tx/${event.transactionHash}\n\n${parsedTokenURI.image}`,
+            embeds: [
+                {
+                    thumbnail: {
+                        url: parsedTokenURI.image,
+                    }
+                }
+            ]
+        });
     });
 
     console.log("NudeBot Ready!");
