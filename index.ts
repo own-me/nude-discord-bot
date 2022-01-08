d9845892bd9b9224f2ad0d1aff4603bc0eeb797c
Christopher Trimboli <christopher.trimboli@protonmail.com>
2022-01-08 01:41:27 -0800

    wrap try catches, slice content limit for discord messages

--- index.ts
+++ index.ts
@@ -21,7 +21,11 @@ const CHANNEL_IDS = {
 
 client.once("ready", () => {
     client.on('guildMemberAdd', (member) => {
-        member.send(`Hey qt, I am NudeBot! The custom bot for Own Me and our $NUDE token. Happy to have you here! Don't be shy!\n\nPlease take my welcome package to get you started in our kinky and wild NFT marketplace. I'm sure you'll find something to peak your interest! :stuck_out_tongue_winking_eye:\n\nhttps://ownme.io\n\nBye now! See you around in the xxx-metaverse! :candy:`);
+        try {
+            member.send(`Hey qt, I am NudeBot! The custom bot for Own Me and our $NUDE token. Happy to have you here! Don't be shy!\n\nPlease take my welcome package to get you started in our kinky and wild NFT marketplace. I'm sure you'll find something to peak your interest! :stuck_out_tongue_winking_eye:\n\nhttps://ownme.io\n\nBye now! See you around in the xxx-metaverse! :candy:`);
+        } catch (e) {
+            console.log(e);
+        }
     });
 
     client.on('messageCreate', (message) => {
@@ -32,17 +36,21 @@ client.once("ready", () => {
     });
 
     contract.on(contract.filters.newNFTMinted(), (recipient, tokenId, tokenURI, price, event) => {
-        const parsedTokenURI = JSON.parse(tokenURI);
-        (client?.channels?.cache?.get(CHANNEL_IDS.nudeBot) as TextChannel)?.send({
-            content: `**${parsedTokenURI.title}**\n\n${parsedTokenURI.description}\n\n${ethers.utils.formatEther(price)} NUDE\n\nhttps://mumbai.polygonscan.com/tx/${event.transactionHash}\n\n${parsedTokenURI.image}`,
-            embeds: [
-                {
-                    thumbnail: {
-                        url: parsedTokenURI.image,
+        try {
+            const parsedTokenURI = JSON.parse(tokenURI);
+            (client?.channels?.cache?.get(CHANNEL_IDS.nudeBot) as TextChannel)?.send({
+                content: `**${parsedTokenURI.title}**\n\n${parsedTokenURI.description.slice(0, 1999)}\n\n${ethers.utils.formatEther(price)} NUDE\n\nhttps://mumbai.polygonscan.com/tx/${event.transactionHash}\n\n${parsedTokenURI.image}`,
+                embeds: [
+                    {
+                        thumbnail: {
+                            url: parsedTokenURI.image,
+                        }
                     }
-                }
-            ]
-        });
+                ]
+            });
+        } catch (e) {
+            console.log(e);
+        }
     });
 
     console.log("NudeBot Ready!");
